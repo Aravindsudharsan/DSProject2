27387307381df717f2cb7231630a8b8669b00d35
Aravindsudharsan <33044292+Aravindsudharsan@users.noreply.github.com>
2017-10-30 15:26:19 -0700

    Add files via upload

--- clientnew.py
+++ clientnew.py
@@ -9,9 +9,12 @@ array_client=[]
 #array_client1=[]
 BUFFER_SIZE = 2000
 #data=[]
+money1=1000
+
 
 id1=raw_input("Enter the process id:")
 def client_thread(ip,port):
+	global money1
 	tcpClient = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 	time.sleep(delay)
 	#if != config["client_details"][int(id1)]["port"]:	
@@ -20,24 +23,29 @@ def client_thread(ip,port):
 	print "[+] New server socket thread started for " + ip + ":" + str(port)
 	#MESSAGE="Hello"
 	#tcpClient.send(MESSAGE)
-	MESSAGE = raw_input("Enter message to send to server:/ Enter exit:")#added
+	MESSAGE = raw_input("Enter the amount to send to a client:/ Enter exit:")#added
 	#while MESSAGE != 'exit':#added
+	money1=money1-int(MESSAGE)
+	print money1
 	tcpClient.send(MESSAGE)#added
-	data1 = tcpClient.recv(BUFFER_SIZE)#added
-	print " Client received data:", data#added"""
+	#data1 = tcpClient.recv(BUFFER_SIZE)#added
+	#print " Client received data:", data#added"""
 
 
 def server_thread(conn):
+	global money1
 	print "Connection message"
 	print conn
 	conn=conn[0]
 	print "Connection established"
 	data=conn.recv(2048)
 	print "Server received data:", data
-	MESSAGE1 = raw_input("Enter response to send to client/Enter exit:")#added
+	money1=money1+int(data)
+	print money1	
+	#MESSAGE1 = raw_input("Enter response to send to client/Enter exit:")#added
 	#if MESSAGE == 'exit':#added
 	#	break#added
-	conn.send(MESSAGE1)#added
+	#conn.send(MESSAGE1)#added
 
 with open("config.json", "r") as configFile:
     config = json.load(configFile)
